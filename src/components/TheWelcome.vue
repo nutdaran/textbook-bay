bf778ebf70475f806632d83d1676748cc86aec00
nutdaran <78150887+nutdaran@users.noreply.github.com>
2023-11-09 15:13:39 +0700

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,40 @@
+<script setup>
+//import { ref } from 'vue'
+
+defineProps({
+  book: {
+    type: Object,
+    required: true,
+  }
+})
+
+// const book = ref({
+//   isbn: 1001,
+//   title: 'CSS for fun',
+//   condition: 'Like New',
+//   published: 'Feb 2, 2020',
+//   price: 400,
+//   freeShipping: true,
+// })
+</script>
+
+<template>
+  <div class="product-card">
+    <h2>{{ book.title }}</h2>
+    <span>Price: {{ book.price }} B , Condition: {{ book.condition }}</span>
+  </div>
+</template>
+
+<style scoped>
+.product-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.product-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/BookListView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import ProductCard from '@/components/ProductCard.vue'
+// import axios from 'axios'
+import BookService from '../services/BookService';
+
+const books = ref(null)
+
+onMounted(() => {
+  // axios.get('https://my-json-server.typicode.com/nutdaran/webapp-test/books')
+  // .then((response)=> {
+  //   books.value = response.data //keep data in books
+  // })
+  // .catch((error) => {
+  //   console.log(error)
+  // })
+    BookService.getBooks()
+  .then((response)=> {
+    books.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
+</script>
+
+<template>
+  <div class="products">
+    <ProductCard v-for="book in books" :key="book.id" :book="book" /> <!--display-->
+  </div>
+</template>
